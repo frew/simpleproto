ddb27bac38b67633b0c5a63bb0688de8881f22ac
frewstore <frewstore@bfd7eaae-ac5c-11dd-a5b1-1f8032f4537c>
2008-11-09 23:44:29 +0000

    Working on configure stuff

---
+++ trunk/test_graphics_client.cpp
@@ -0,0 +1,16 @@
+#include "graphics.pb.h"
+#include "rpc.h"
+
+int main(int argc, char** argv) {
+  RPC* rpc = RPC::CreateClient("localhost", "1234");
+  GraphicsTransaction t;
+  LineMessage* m = t.add_message()->mutable_line();
+  m->set_x0(0.0);
+  m->set_x1(1.0);
+  m->set_y0(0.0);
+  m->set_y1(1.0);
+  string foo;
+  t.SerializeToString(&foo);
+  rpc->SendMessage(foo, false);
+  return 0;
+}
---
+++ trunk/test_rpc_server.cpp
@@ -0,0 +1,12 @@
+#include "rpc.h"
+
+int main(int argc, char** argv) {
+  RPC* rpc = RPC::CreateServer("1234");
+  while (true) {
+    string* msg = rpc->PollMessage(true);
+    if (msg) {
+      cout << *msg << endl;
+    }
+  }
+  return 0;
+}
